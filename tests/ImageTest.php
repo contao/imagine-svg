d5efc0753815bb0528b9b2ea2379beb91b2737d2
Martin Auswöger <martin@auswoeger.com>
2016-08-08 23:34:08 +0200

    Add tests for unimplemented methods of Image

--- tests/ImageTest.php
+++ tests/ImageTest.php
@@ -384,4 +384,208 @@ class ImageTest extends \PHPUnit_Framework_TestCase
             'invalid' => ['abc', 0],
         ];
     }
+
+    /**
+     * Tests the paste() method.
+     */
+    public function testPaste()
+    {
+        $image = new Image(new \DOMDocument(), new MetadataBag());
+
+        $this->setExpectedException('Imagine\Exception\RuntimeException');
+
+        $image->paste(new Image(new \DOMDocument(), new MetadataBag()), new Point(0, 0));
+    }
+
+    /**
+     * Tests the rotate() method.
+     */
+    public function testRotate()
+    {
+        $image = new Image(new \DOMDocument(), new MetadataBag());
+
+        $this->setExpectedException('Imagine\Exception\RuntimeException');
+
+        $image->rotate(90);
+    }
+
+    /**
+     * Tests the flipHorizontally() method.
+     */
+    public function testFlipHorizontally()
+    {
+        $image = new Image(new \DOMDocument(), new MetadataBag());
+
+        $this->setExpectedException('Imagine\Exception\RuntimeException');
+
+        $image->flipHorizontally();
+    }
+
+    /**
+     * Tests the flipVertically() method.
+     */
+    public function testFlipVertically()
+    {
+        $image = new Image(new \DOMDocument(), new MetadataBag());
+
+        $this->setExpectedException('Imagine\Exception\RuntimeException');
+
+        $image->flipVertically();
+    }
+
+    /**
+     * Tests the strip() method.
+     */
+    public function testStrip()
+    {
+        $image = new Image(new \DOMDocument(), new MetadataBag());
+
+        $this->setExpectedException('Imagine\Exception\RuntimeException');
+
+        $image->strip();
+    }
+
+    /**
+     * Tests the draw() method.
+     */
+    public function testDraw()
+    {
+        $image = new Image(new \DOMDocument(), new MetadataBag());
+
+        $this->setExpectedException('Imagine\Exception\RuntimeException');
+
+        $image->draw();
+    }
+
+    /**
+     * Tests the effects() method.
+     */
+    public function testEffects()
+    {
+        $image = new Image(new \DOMDocument(), new MetadataBag());
+
+        $this->setExpectedException('Imagine\Exception\RuntimeException');
+
+        $image->effects();
+    }
+
+    /**
+     * Tests the applyMask() method.
+     */
+    public function testApplyMask()
+    {
+        $image = new Image(new \DOMDocument(), new MetadataBag());
+
+        $this->setExpectedException('Imagine\Exception\RuntimeException');
+
+        $image->applyMask(new Image(new \DOMDocument(), new MetadataBag()));
+    }
+
+    /**
+     * Tests the fill() method.
+     */
+    public function testFill()
+    {
+        $image = new Image(new \DOMDocument(), new MetadataBag());
+
+        $this->setExpectedException('Imagine\Exception\RuntimeException');
+
+        $image->fill($this->getMock('Imagine\Image\Fill\FillInterface'));
+    }
+
+    /**
+     * Tests the mask() method.
+     */
+    public function testMask()
+    {
+        $image = new Image(new \DOMDocument(), new MetadataBag());
+
+        $this->setExpectedException('Imagine\Exception\RuntimeException');
+
+        $image->mask();
+    }
+
+    /**
+     * Tests the histogram() method.
+     */
+    public function testHistogram()
+    {
+        $image = new Image(new \DOMDocument(), new MetadataBag());
+
+        $this->setExpectedException('Imagine\Exception\RuntimeException');
+
+        $image->histogram();
+    }
+
+    /**
+     * Tests the getColorAt() method.
+     */
+    public function testGetColorAt()
+    {
+        $image = new Image(new \DOMDocument(), new MetadataBag());
+
+        $this->setExpectedException('Imagine\Exception\RuntimeException');
+
+        $image->getColorAt(new Point(0, 0));
+    }
+
+    /**
+     * Tests the layers() method.
+     */
+    public function testLayers()
+    {
+        $image = new Image(new \DOMDocument(), new MetadataBag());
+
+        $this->setExpectedException('Imagine\Exception\RuntimeException');
+
+        $image->layers();
+    }
+
+    /**
+     * Tests the interlace() method.
+     */
+    public function testInterlace()
+    {
+        $image = new Image(new \DOMDocument(), new MetadataBag());
+
+        $this->setExpectedException('Imagine\Exception\RuntimeException');
+
+        $image->interlace('');
+    }
+
+    /**
+     * Tests the palette() method.
+     */
+    public function testPalette()
+    {
+        $image = new Image(new \DOMDocument(), new MetadataBag());
+
+        $this->setExpectedException('Imagine\Exception\RuntimeException');
+
+        $image->palette();
+    }
+
+    /**
+     * Tests the profile() method.
+     */
+    public function testProfile()
+    {
+        $image = new Image(new \DOMDocument(), new MetadataBag());
+
+        $this->setExpectedException('Imagine\Exception\RuntimeException');
+
+        $image->profile($this->getMock('Imagine\Image\ProfileInterface'));
+    }
+
+    /**
+     * Tests the usePalette() method.
+     */
+    public function testUsePalette()
+    {
+        $image = new Image(new \DOMDocument(), new MetadataBag());
+
+        $this->setExpectedException('Imagine\Exception\RuntimeException');
+
+        $image->usePalette($this->getMock('Imagine\Image\Palette\PaletteInterface'));
+    }
 }
